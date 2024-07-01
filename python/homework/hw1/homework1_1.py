bbe35b0955dc224f9ab7a3258cc08d7c2e273dda
KyryloMuzychka <kyryll2016@gmail.com>
2024-07-01 21:47:31 +0300

    python hw1

---
+++ python/homework/hw1/homework1_1.py
@@ -0,0 +1,3 @@
+from colorama import Fore
+
+print("To be\n" + Fore.RED + "or not\n" + Fore.RESET + "to be")
---
+++ python/homework/hw1/homework1_2.py
@@ -0,0 +1,3 @@
+from colorama import Fore
+
+print(Fore.LIGHTYELLOW_EX + "\"Life is what happens\nwhen\nyou\'are busy making other plans\"" + Fore.RESET + "\nJohn Lennon")
